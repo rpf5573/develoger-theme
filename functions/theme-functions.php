64a924c8fd6f2c1a02db90dd4275bcc22198bb9e
comseung18 <comseung18@koreatech.ac.kr>
2020-12-26 08:44:55 +0900

    Working in theme-function

--- functions/theme-functions.php
+++ functions/theme-functions.php
@@ -3,8 +3,51 @@
 function develoger_pagination($query = null) {
   global $wp_query, $paged;
   if ( $query ) $wp_query = $query;
-  if ( !empty($wp_query->query['paged']) ) {
-    $paged = $wp_query->query['paged'];
-  } ?>
-  
+  get_template_part('partials/content', 'pagination', $wp_query);
+}
+
+add_filter('wp_list_categories', 'epcl_at_count_span');
+function develoger_add_count_to_category_links($links) {
+  $links = str_replace('</a> (', '</a> <span class="count"', $links);
+  $links = str_replace(')', '</span>', $links);
+  return $links;
+}
+
+add_filter('get_archives_link', 'epcl_archives_count');
+function develoger_add_count_to_archive_linkes($links) {
+  $links = str_replace('</a>&nbsp;(', '</a> <span class="count">', $links);
+  $links = str_replace(')</li>', '</span></li>', $links);
+  return $links;
+}
+
+// Add search botton to the end of the main menu
+function develoger_add_search_btn($items, $args) {
+  if ($args->theme_location != 'develoger_header') { return $items; }
+  ob_start();
+  get_template_part('partials/search-btn');
+  $search_btn = ob_get_contents();
+  ob_end_clean();
+  return ($items . $search_btn);
+}
+
+// Render categories in single post
+function develoger_render_categories($class = 'absolute') {
+  $categories = get_the_category();
+  $html = '<div class="tags '.$class.'">';
+  foreach($categories as $cat) {
+    $html .= '<a href="'.get_category_link($cat).'" class="tag-link-'.$cat->term_id.'">'.$cat->name.'</a>';
+  }
+  $html .= '</div>';
+  return $html;
+}
+
+function develoger_reading_time($content) {
+  if (!$content) return;
+
+  $words_per_minute = 200;
+  $word_per_second = $words_per_minute / 60;
+  $words = str_word_count( strip_tags( $content ), 0 );
+  $minutes = 1;
+  if (!empty($words)) $minutes = max([1, floor($words, $words_per_minute)]);
+  return $minutes;
 }
---
+++ partials/search-btn.php
@@ -0,0 +1,7 @@
+<li class="search-menu-item hide-on-mobile hide-on-tablet">
+  <a href="#search-lightbox" class="lightbox mfp-inline">
+    <svg class="icon">
+      <use xlink:href="#search-icon"></use>
+    </svg>
+  </a>
+</li>;
