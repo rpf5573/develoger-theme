0c925544cabf811aa86bd2cf1bcbd6fe8531037e
comseung18 <comseung18@koreatech.ac.kr>
2020-12-25 15:43:36 +0900

    save

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<main id="home" class="main">
-    
+<main id="home" class="main"> <?php
+  get_template_part('partials/home-blocks/classic-posts'); ?>
 </main>
 
 <?php get_footer(); ?>
